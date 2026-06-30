4c78716b754fcbc50dbec52e5ac5a9c42f7dacc6
tqsk <sheheryar0789@gmail.com>
2026-06-30 12:01:47 +0500

    final build

--- main.py
+++ main.py
@@ -26,6 +26,11 @@ def main():
         - Write content to a file (create or update)
         - Run a Python file with optional command-line arguments
 
+        When the user asks about the code project - they are referring to 
+        the working directory. So, you should typically start by looking at the 
+        project's files, and figuring out how to run the project and how
+        to run its tests, you'll always want to test the tests and the actual project
+        to verify that behavior is working.
         All paths you provide should be relative to the working directory. You do not need to specify the working directory in your function calls as it is automatically injected for security reasons.
         """
 
